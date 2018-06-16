6578a77210e1fcde2fdc6a5b9069ca964397571b
roberto <roberto@roberto-Lenovo-ideapad-320-15IKB>
2018-06-16 18:52:34 -0300

    A primeira rota com Camel

---
+++ src/main/java/br/com/rbaselio/camel/RotaPedidos.java
@@ -0,0 +1,31 @@
+package br.com.rbaselio.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class RotaPedidos {
+
+	public static void main(String[] args) throws Exception {
+
+		CamelContext context = new DefaultCamelContext();
+
+		context.addRoutes(new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+				from("file:pedidos?delay=5s&noop=true").
+				log("${id}").
+				marshal().xmljson().
+				log("${id} - ${body}").
+				setHeader("CamelFileName", simple("${file:name.noext}.json")).
+				to("file:saida");
+			}
+		});
+
+		context.start(); // aqui camel realmente começa a trabalhar
+		Thread.sleep(20000); // esperando um pouco para dar um tempo para camel
+		context.stop();
+
+	}
+}
